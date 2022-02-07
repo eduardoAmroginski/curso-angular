38d4487dd07b1ca05d143dcb7ef74cee0bb83409
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2022-02-07 16:06:25 -0300

    feat(reviews): adicionado Aula 48, componente reviews e rotas filhas de restaurant-detail

---
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/Aulas/Aula 48 - Rotas Filhas para Avaliação e Menu.md
@@ -0,0 +1,7 @@
+# Rotas Filhas para Avaliação e Menu
+
+Coisas realizadas nesta aula.
+
+1. Criado o componente **reviews**
+2. Criada as rotas filhas de **restaurant-detail**
+3. Arquivo **restaurant-detail.component.html** refatorado para utilizar o **router-outlet** onde será inserido os componentes das rotas filhas.
--- Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/app.routes.ts
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from "@angular/router"; //Tipo padrão para rotas do angular
+
 import { HomeComponent } from "./home/home.component";
 import { RestaurantsComponent } from "./restaurants/restaurants.component";
-import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { AboutComponent } from "./about/about.component";
+
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
+import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'restaurants', component: RestaurantsComponent },
-    { path: 'restaurants/:id', component: RestaurantDetailComponent },
+    { path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            {path: '', redirectTo: 'menu', pathMatch: 'full' }, //Define a rota filha padrão a ser acessada
+            {path: 'menu', component: MenuComponent},
+            {path: 'reviews', component: ReviewsComponent}
+        ]},
     { path: 'about', component: AboutComponent }
 ]

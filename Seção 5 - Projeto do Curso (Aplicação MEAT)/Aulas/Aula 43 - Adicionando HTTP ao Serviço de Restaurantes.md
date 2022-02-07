508418c4b88b4bb35fc104ba4726172dcaf10006
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2022-02-07 12:33:41 -0300

    refactor(restaurants): adicionando aula 43 e refatorando código de restaurants

---
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/Aulas/Aula 43 - Adicionando HTTP ao Serviço de Restaurantes.md
@@ -0,0 +1,7 @@
+# Adicionando HTTP ao Serviço de Restaurantes
+
+Nessa aula foram criados as seguites coisas
+
+1. Criado arquivo **app.api.ts** arquivo este que exporta a constante _MEAT_API_
+2. Arquivo **restaurants.service.ts** foi refatorado para usar o mock.
+3. Adicionado subscribe
--- Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/restaurants/restaurants.service.ts
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/restaurants/restaurants.service.ts
@@ -1,37 +1,19 @@
-import { Restaurant } from "./card-restaurant/restaurant.model";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map'
 
+import { Restaurant } from "./card-restaurant/restaurant.model";
+import { MEAT_API } from "app/app.api";
+
+@Injectable()
 export class RestaurantsService {
 
-      //Passando um array de interface Restaurant
-  rests: Restaurant[] = [ {
-    id: "bread-bakery",
-    name: "Bread & Bakery",
-    category: "Bakery",
-    deliveryEstimate: "25m",
-    rating: 4.9,
-    imagePath: "assets/img/restaurants/breadbakery.png"
-  },
-  {
-    id: "burger-house",
-    name: "Burger House",
-    category: "Hamburgers",
-    deliveryEstimate: "100m",
-    rating: 3.5,
-    imagePath: "assets/img/restaurants/burgerhouse.png"
-  },
-  {
-    id: "coffee-corner",
-    name: "Coffee Corner",
-    category: "Coffee Shop",
-    deliveryEstimate: "30-40m",
-    rating: 4.8,
-    imagePath: "assets/img/restaurants/coffeecorner.png"
-  }]
-    constructor(){
-        
-    }
 
-    restaurants(): Restaurant[]{
-        return this.rests
+    constructor(private http: Http){}
+
+    restaurants(): Observable<Restaurant[]>{
+        return this.http.get(`${MEAT_API}/restaurants`)
+          .map(response => response.json())
     }
 }

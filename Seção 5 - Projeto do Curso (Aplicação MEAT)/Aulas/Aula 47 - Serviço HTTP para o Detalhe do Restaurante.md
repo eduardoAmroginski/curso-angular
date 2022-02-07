36bcd149ca6c590307d48c4928e6513d3940f150
Eduardo Amroginski Santos <eduardo-amroginski@ibm.com>
2022-02-07 15:48:53 -0300

    feat(restaurant-detail): criada nova chamada para API e refatoração de código

---
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/Aulas/Aula 47 - Serviço HTTP para o Detalhe do Restaurante.md
@@ -0,0 +1,7 @@
+# Serviço HTTP para o Detalhe do Restaurante
+
+Coisas realizadas nesta aula.
+
+1. No **restaurant.service.ts** foi criado uma nova chamada para o backend chamada de **restaurantById**
+2. Chamando **restaurantById** em **restaurant-detail.component.ts**
+3. Refatorando o arquivo **restaurant-detail.component.html** para usar os dados dinamicos obtidos através do **restaurantById**
--- Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/restaurant-detail/restaurant-detail.component.ts
+++ Seção 5 - Projeto do Curso (Aplicação MEAT)/meat-app/meat-app-starter-2/src/app/restaurant-detail/restaurant-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Restaurant } from 'app/restaurants/card-restaurant/restaurant.model';
+
+import { RestaurantsService } from 'app/restaurants/restaurants.service';
 
 @Component({
   selector: 'mt-restaurant-detail',
@@ -6,9 +11,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantDetailComponent implements OnInit {
 
-  constructor() { }
+  restaurant: Restaurant
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+      .subscribe(restaurant => this.restaurant = restaurant)
+
   }
 
 }
